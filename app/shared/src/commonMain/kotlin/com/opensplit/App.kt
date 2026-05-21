4a15105b738f75aa53e8b8956426a27cb9286260
Amir Hossein Parhizgar <ahparhizgar@gmail.com>
2026-05-22 02:31:40 +0330

    Somehow works

--- app/shared/src/commonMain/kotlin/com/opensplit/App.kt
+++ app/shared/src/commonMain/kotlin/com/opensplit/App.kt
@@ -2,28 +2,31 @@ package com.opensplit
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.opensplit.features.auth.AuthComponent
 import com.opensplit.features.auth.AuthRootScreen
 import com.opensplit.features.auth.FakeAuthComponent
+import com.opensplit.root.RootComponent
 
 @Composable
-fun App(root: AuthComponent) {
+fun App(root: RootComponent) {
     MaterialTheme {
         // Create a minimal Decompose CContext for the root component and use
         // the Decompose-based AuthComponent directly in the UI.
-        AuthRootScreen(
-            component = root,
-            modifier = Modifier.fillMaxSize(),
-        )
+        Text(root::class.simpleName.orEmpty())
+//        AuthRootScreen(
+//            component = root,
+//            modifier = Modifier.fillMaxSize(),
+//        )
     }
 }
 
 @Preview
 @Composable
 private fun Preview() {
-    App(FakeAuthComponent())
+//    App(FakeRootComponent())
 }
 
