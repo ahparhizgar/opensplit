a61d6e263475a116408bd606b38b28f370bac45a
Amir Hossein Parhizgar <ahparhizgar@gmail.com>
2026-06-24 16:14:41 +0330

    Fix back press in desktop

--- app/desktopApp/src/main/kotlin/com/opensplit/main.kt
+++ app/desktopApp/src/main/kotlin/com/opensplit/main.kt
@@ -1,16 +1,23 @@
 package com.opensplit
 
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.arkivanov.decompose.DecomposeSettings
 import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
+import com.arkivanov.essenty.backhandler.BackDispatcher
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.opensplit.component.DefaultCContext
 import com.opensplit.root.RootComponent
 import org.koin.core.context.GlobalContext.startKoin
 import javax.swing.SwingUtilities
-import androidx.compose.ui.unit.dp
 
 fun main() {
     val lifecycle = LifecycleRegistry()
@@ -20,19 +27,39 @@ fun main() {
             modules(appModule())
         }.koin
 
+    val backDispatcher = BackDispatcher()
+    val context = DefaultCContext(lifecycle = lifecycle, backHandler = backDispatcher)
     val root = runOnUiThread {
-        koin.get<RootComponent.Factory>().create(DefaultCContext(lifecycle = lifecycle))
+        koin.get<RootComponent.Factory>().create(context)
     }
     DecomposeSettings.settings = DecomposeSettings(duplicateConfigurationsEnabled = true)
     application {
-        val windowState = rememberWindowState(width = 1280.dp, height = 900.dp)
+        val windowState = rememberWindowState(width = 400.dp, height = 800.dp)
         LifecycleController(lifecycle, windowState)
-
         Window(
             onCloseRequest = ::exitApplication,
             title = "OpenSplit",
+            onKeyEvent = {
+                if (it.key == Key.Escape && it.type == KeyEventType.KeyDown) {
+                    backDispatcher.back()
+                    true
+                } else {
+                    false
+                }
+            },
+            state = windowState,
         ) {
-            App(root)
+            MenuBar {
+                Menu("Navigation") {
+                    Item(
+                        "Back",
+                        shortcut = KeyShortcut(Key.Escape)
+                    ) {
+                        backDispatcher.back()
+                    }
+                }
+            }
+            App(root = root)
         }
     }
 }
--- app/shared/src/commonMain/kotlin/com/opensplit/App.kt
+++ app/shared/src/commonMain/kotlin/com/opensplit/App.kt
@@ -7,21 +7,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.opensplit.features.auth.AuthComponent
 import com.opensplit.features.auth.AuthRootScreen
-import com.opensplit.features.household.root.RootHouseholdComponent
+import com.opensplit.features.household.createjoin.CreateJoinHouseholdComponent
+import com.opensplit.features.household.createjoin.CreateJoinHouseholdScreen
 import com.opensplit.features.household.root.HouseholdRootScreen
+import com.opensplit.features.household.root.RootHouseholdComponent
 import com.opensplit.root.FakeRootComponent
 import com.opensplit.root.RootComponent
 import com.opensplit.ui.OpenSplitTheme
 
 @Composable
-fun App(root: RootComponent) {
+fun App(root: RootComponent, modifier: Modifier = Modifier) {
     OpenSplitTheme {
-        Children(root.childStack) {
+        Children(
+            modifier = modifier,
+            stack = root.childStack
+        ) {
             when (val child = it.instance) {
                 is AuthComponent -> AuthRootScreen(
                     component = child,
                     modifier = Modifier.fillMaxSize(),
                 )
+
+                is CreateJoinHouseholdComponent -> CreateJoinHouseholdScreen(
+                    component = child,
+                    modifier = Modifier.fillMaxSize(),
+                )
+
                 is RootHouseholdComponent -> HouseholdRootScreen(
                     component = child,
                     modifier = Modifier.fillMaxSize(),
