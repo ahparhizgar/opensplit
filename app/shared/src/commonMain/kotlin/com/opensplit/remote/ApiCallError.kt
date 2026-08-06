cd954cfca530aca4f2dd4c5a0209b1858d836675
AmirHossein Parhizgar <amirhossein.parhizgar@snapp.cab>
2026-08-06 20:46:21 +0330

    Improve ApiCallError.userMessage

--- app/shared/src/commonMain/kotlin/com/opensplit/remote/ApiCallError.kt
+++ app/shared/src/commonMain/kotlin/com/opensplit/remote/ApiCallError.kt
@@ -1,10 +1,45 @@
 package com.opensplit.remote
 
 import com.ahparhizgar.katch.ApiCallError
+import com.ahparhizgar.katch.BadRequest
 import com.ahparhizgar.katch.ClientError
+import com.ahparhizgar.katch.Forbidden
+import com.ahparhizgar.katch.HttpError
+import com.ahparhizgar.katch.InvalidDataError
+import com.ahparhizgar.katch.NetworkError
+import com.ahparhizgar.katch.NotFound
+import com.ahparhizgar.katch.RateLimitReached
+import com.ahparhizgar.katch.ServerError
+import com.ahparhizgar.katch.Unauthorized
 
+@Suppress("UNCHECKED_CAST")
 val ApiCallError.fieldErrors: Map<String, String>
-  get() = (this.payload as? ClientError)?.fieldErrors ?: emptyMap()
+  get() = payload as? Map<String, String> ?: emptyMap()
 
-val ApiCallError.userMessage: String?
-  get() = (this as? ClientError)?.userMessage
+val ApiCallError.userMessage: String
+  get() {
+    val baseMessage =
+        when (this) {
+          is ClientError ->
+              userMessage
+                  ?: when (this) {
+                    is BadRequest -> "Invalid request"
+                    is Unauthorized -> "Unauthorized access"
+                    is Forbidden -> "Access denied"
+                    is NotFound -> "Content not found"
+                    is RateLimitReached -> "Too many requests"
+                    else -> "A client error occurred"
+                  }
+          is NetworkError -> "Internet is disconnected"
+          is ServerError -> "An error occurred on our side"
+          is InvalidDataError -> message ?: "An error occurred during processing of data"
+          else -> message ?: "An unknown error occurred"
+        }
+
+    return if (this is HttpError) {
+      val debugInfo = " ($code${if (!message.isNullOrBlank()) ": $message" else ""})"
+      baseMessage + debugInfo
+    } else {
+      baseMessage
+    }
+  }
