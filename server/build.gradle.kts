c48a9028dc4157ff243045cbea54c3492f6ed8a7
Amir Hossein Parhizgar <ahparhizgar@gmail.com>
2026-09-16 20:49:29 +0330

    Add migration support

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
   implementation(libs.exposed.core)
   implementation(libs.exposed.dao)
   implementation(libs.exposed.jdbc)
+  implementation(libs.exposed.migration.jdbc)
   implementation(libs.exposed.java.time)
   implementation(libs.hikaricp)
   implementation(libs.postgresql)
--- server/src/main/kotlin/com/opensplit/database/DatabaseInitializer.kt
+++ server/src/main/kotlin/com/opensplit/database/DatabaseInitializer.kt
@@ -1,20 +1,29 @@
 package com.opensplit.database
 
 import org.jetbrains.exposed.v1.jdbc.Database
-import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.jetbrains.exposed.v1.migration.jdbc.MigrationUtils
 
 class DatabaseInitializer(private val database: Database) {
   fun init() {
     transaction(database) {
-      SchemaUtils.createMissingTablesAndColumns(
-          ChangeLog,
-          Users,
-          Households,
-          Memberships,
-          Expenses,
-          ExpenseParticipants,
-      )
+      val statements =
+          MigrationUtils.statementsRequiredForDatabaseMigration(
+              ChangeLog,
+              Users,
+              Households,
+              Memberships,
+              Expenses,
+              ExpenseParticipants,
+              withLogs = true,
+          )
+
+      for (stmt in statements) {
+        if (stmt.trimStart().startsWith("DROP ", ignoreCase = true)) {
+          continue
+        }
+        exec(stmt)
+      }
     }
   }
 }
